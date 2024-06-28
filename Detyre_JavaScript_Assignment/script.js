a7dc9b323507fbf09e278b091a70ce5cb5f46e74
florentsahiti <66284617+florentsahiti@users.noreply.github.com>
2024-06-28 11:52:52 +0200

    Detyra ne Javascript part 1

---
+++ Detyre_JavaScript_Assignment/script.js
@@ -0,0 +1,19 @@
+function withdraw(person, amount) {
+    
+    if (amount > person.balance) {
+        console.log("Withdrawal cannot be processed due to insufficient funds.");
+    } else {
+        
+        person.balance -= amount;
+        console.log(`Withdrawal successful. New balance: ${person.balance}`);
+    }
+}
+
+
+let person = {
+    name: "John Doe",
+    balance: 1000
+};
+
+withdraw(person, 500);  
+withdraw(person, 600);  
---
+++ Java2(JS)/script.js
@@ -0,0 +1,27 @@
+var florent = "This is my Name";
+console.log(florent);
+
+// objekt
+var person = {
+    name:"Florent",
+    lastname:"Sahiti",
+    age: 21
+}
+
+console.log(person)
+
+
+var array = [1,2,3,4,5]
+console.log(array[4])
+
+var myName = "My name is: ";
+var firstname= "Klea";
+var lastname= "Manushi";
+
+console.log(myName+firstname+ " "  +lastname)
+
+for(let i = 0; i <= 10; i++){
+    if(i%2){
+        console.log(i)
+    }
+}
